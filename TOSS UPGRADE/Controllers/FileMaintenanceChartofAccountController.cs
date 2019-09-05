de259b69a9bfe2bbaa13ac12444e9a10e1b888aa
area69 <christian.aquino53@gmail.com>
2019-09-05 13:21:06 +0800

    Wireframe Update ( Based on Commonality ) (Chart of Accounts, Position, Responsibility Center and Signatory)

---
+++ TOSS UPGRADE/Controllers/FileMaintenanceChartofAccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TOSS_UPGRADE.Controllers
+{
+    public class FileMaintenanceChartofAccountController : Controller
+    {
+        // GET: FileMaintenanceChartofAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
